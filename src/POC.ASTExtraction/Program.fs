d1ffd02b923d8adf2d580ef65ee8c73cc291da48
Dave Thomas <kukulcanenator@googlemail.com>
2018-01-09 23:30:38 +0000

    Added summary parsetree just to get a feel for it

--- src/POC.ASTExtraction/Program.fs
+++ src/POC.ASTExtraction/Program.fs
@@ -3,6 +3,9 @@
 open System
 open System.IO
 open Microsoft.FSharp.Compiler.SourceCodeServices
+open Microsoft.FSharp.Compiler.Ast
+open System.Runtime.InteropServices.WindowsRuntime
+open System.Xml.Linq
 
 let checker = FSharpChecker.Create(keepAssemblyContents=true, ImplicitlyStartBackgroundWork=false)
 
@@ -19,7 +22,6 @@ let dependencies =
             let aa = System.Reflection.Assembly.Load(a.FullName)
             yield sprintf "-r:%s" aa.Location ]
 
-printfn "%A" dependencies
 let arguments =
     [|
         //yield "--simpleresolution"
@@ -34,7 +36,6 @@ let arguments =
         yield! dependencies
         yield fileName |]
 
-printfn "arguments:\n%A" arguments
 let projectOptions =
             
             checker.GetProjectOptionsFromCommandLineArgs(projectFilename, arguments)
@@ -51,7 +52,78 @@ let parseFileResults = checker.ParseFile(fileName, input, parsingOptions) |> Asy
 let parseTree = 
     match parseFileResults.ParseTree with
     | Some tree -> tree
-    | None -> failwith "Something went wrong during parsing!"
+    | None -> failwith "Something went wrong during parsing.  No parseTree!"
+
+let rec visitPattern (node: SynPat) =
+  printf "Node: %s " (node.GetType().Name)
+  match node with
+  | SynPat.Wild(_range) -> 
+      printfn "underscore pattern"
+  | SynPat.Named(pat, name, _, _, _range) ->
+      visitPattern pat
+      printfn "named: '%s'" name.idText
+  | SynPat.LongIdent(LongIdentWithDots(ident, _), _, _, _, _, _) ->
+      let names = String.concat "." [ for i in ident -> i.idText ]
+      printfn "identifier: %s" names
+  | pat ->
+      printfn "other pattern: %A" pat
+
+let rec visitExpression (node: SynExpr) =
+  printf "Node: %s " (node.GetType().Name)
+  match node with
+  | SynExpr.IfThenElse(cond, trueBranch, falseBranchOpt, _, _, _, _) ->
+      // Visit all sub-expressions
+      printfn "Conditional:"
+      visitExpression cond
+      visitExpression trueBranch
+      falseBranchOpt |> Option.iter visitExpression 
+
+  | SynExpr.LetOrUse(_, _, bindings, body, _) ->
+      // Visit bindings (there may be multiple 
+      // for 'let .. = .. and .. = .. in ...'
+      printfn "LetOrUse with the following bindings:"
+      for binding in bindings do
+        let (Binding(access, kind, inlin, mutabl, attrs, xmlDoc, data, pat, retInfo, init, m, sp)) = binding
+        visitPattern pat 
+        visitExpression init
+      // Visit the body expression
+      printfn "And the following body:"
+      visitExpression body
+  | SynExpr.Const(cst, _range) ->
+      printfn "Const: %A" cst
+  | SynExpr.App(exprAtomicFlag, isInfix, funExpr, argExpr, _range) ->
+        (visitExpression funExpr)
+        (visitExpression argExpr)
+     
+  | SynExpr.Ident(ident) ->
+      printfn "Ident: %A" ident
+  | expr -> printfn "visitExpression - not supported expression: %A" expr
+
+let visitDeclarations decls = 
+  for declaration in decls do
+    printf "Node: %s " (declaration.GetType().Name)
+    match declaration with
+    | SynModuleDecl.Let(isRec, bindings, range) ->
+        // Let binding as a declaration is similar to let binding
+        // as an expression (in visitExpression), but has no body
+        for binding in bindings do
+          let (Binding(access, kind, inlin, mutabl, attrs, xmlDoc, data, pat, retInfo, body, m, sp)) = binding
+          visitPattern pat 
+          visitExpression body         
+    | _ -> printfn "visitDeclarations - not supported declaration: %A" declaration
+
+let visitModulesAndNamespaces modulesOrNss =
+  for moduleOrNs in modulesOrNss do
+    let (SynModuleOrNamespace(lid, isRec, isMod, decls, xml, attrs, _, m)) = moduleOrNs
+    if isMod then printfn "module: %A" lid
+    else printfn "Namespace: %A" lid
+    visitDeclarations decls
+
+let niceParseTree =
+   match parseTree with
+   | ParsedInput.ImplFile(ParsedImplFileInput(filename, isScript, qualifierFilename, scopedPragmas, hashDirectives, modules, (isLastCompiland, isExe))) ->
+       visitModulesAndNamespaces modules
+   | ParsedInput.SigFile _ -> failwith "F# Interface file not supported."
 
 let typedAst =
     checker.ParseAndCheckProject(projectOptions) |> Async.RunSynchronously
@@ -60,6 +132,6 @@ printfn "Errors: %A" typedAst.Errors
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" parseTree
-    printfn "%A" typedAst.AssemblyContents.ImplementationFiles.Head.Declarations
+    printfn "\n%A" parseTree
+    printfn "\n%A" typedAst.AssemblyContents.ImplementationFiles.Head.Declarations
     0 // return an integer exit code
